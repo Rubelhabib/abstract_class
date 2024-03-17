dd89503c5486936ed9589df16efb08b94fa6bef5
Rubelhabib <rubelhabib007@gmail.com>
2024-03-17 16:56:06 +0600

    Add project files.

---
+++ abstract_class/Car_Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    abstract class Car_Model
+    {
+
+        public void Model(int id, string name, float price)
+        {
+            Console.WriteLine($"Id: {id}\n Name is: {name}\n Price: {price}");
+        }
+        public abstract void Sp();
+    }
+
+    class Audi : Car_Model
+    {
+        public override void Sp()
+        {
+            Console.WriteLine("This is brand! ");
+        }
+    }
+        
+}
---
+++ abstract_class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            bus.Speed();
+            bus.highway(12,"Sara",'R');
+            Console.WriteLine();
+            Audi audi = new Audi();
+            audi.Sp();
+            audi.Model(1,"Hebi",12.50f);
+            Console.ReadKey();
+
+        }
+    }
+
+    abstract class Car
+    {
+        public void highway(int rade, string name, char a)
+        {
+            Console.WriteLine($"This is simplemethod in abstactor! \n Rade is: {rade} \n Name is: {name}\n Charachter is: {a}");
+        }
+
+        public abstract void Speed();
+
+    }
+    class Bus : Car
+    {
+        public override void Speed()
+        {
+            Console.WriteLine("Car speed is 80km/h!");
+        }
+    }
+}
